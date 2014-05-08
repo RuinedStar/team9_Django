ff97c4c97da8d4d9861c57c4a31606ae7169093b
pythonanywhere <arthurx94@gmail.com>
2014-05-08 11:30:00 +0000

    fix accounts/login 	modified:   mysite/mysite/accounts/urls.py 	modified:   mysite/mysite/article/views.py 	modified:   mysite/testDB.db

--- mysite/mysite/accounts/urls.py
+++ mysite/mysite/accounts/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('mysite.accounts.views',
 
-	url(r'^accounts/login/$', login),
-	url(r'^accounts/auth/$', auth_view),
-	url(r'^accounts/logout/$', logout),
-	url(r'^accounts/loggedin/$', loggedin),
-	url(r'^accounts/invalid/$', invalid_login),
+	url(r'^login/$', 'login'),
+	url(r'^auth/$', 'auth_view'),
+	url(r'^logout/$', 'logout'),
+	url(r'^loggedin/$', 'loggedin'),
+	url(r'^invalid/$', 'invalid_login'),
 )
--- mysite/mysite/article/views.py
+++ mysite/mysite/article/views.py
@@ -10,6 +10,14 @@ from mysite.article.models import Article
 
 # Create your views here.
 
+def articles(request):
+    return render_to_response('articles.html',
+                             {'articles': Article.objects.all()})
+
+def article(request, article_id=1):
+    return render_to_response('article.html',
+                             {'article': Article.objects.get(id=article_id)})
+
 '''
 def hello(request):
 	name = "Arthur"
@@ -35,13 +43,6 @@ class HelloTemplate(TemplateView):
         return context
 '''
 
-def articles(request):
-    return render_to_response('articles.html',
-                             {'articles': Article.objects.all()})
-
-def article(request, article_id=1):
-    return render_to_response('article.html',
-                             {'article': Article.objects.get(id=article_id)})
 
 
 
