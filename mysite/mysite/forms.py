afae4162fb5dd64ec417de9d3936096ee7e795d0
arthurx94 <arthurx94@gmail.com>
2014-05-15 19:54:57 +0800

    add forms for extend register

---
+++ mysite/mysite/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class MyRegistrationForm(UserCreationForm):
+	email = forms.EmailField(required=True)
+
+	class Meta:
+		model = User
+		fields = ('username', 'email', 'password1', 'password2')
+
+	def save(self, commit=True):
+		user = super(MyRegistrationForm, self).save(commit=False)
