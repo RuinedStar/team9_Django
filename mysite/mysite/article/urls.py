c73d993432bc7616a5cf0de2e46063d5f59b2131
arthurx94 <arthurx94@gmail.com>
2014-05-24 11:31:07 +0800

    set new articles app authentically

--- mysite/mysite/article/urls.py
+++ mysite/mysite/article/urls.py
@@ -4,5 +4,6 @@ urlpatterns = patterns('',
     url(r'^all/$', 'mysite.article.views.articles'),
     url(r'^get/(?P<article_id>\d+)/$', 'mysite.article.views.article'),
     url(r'^create/$', 'mysite.article.views.create'),
+    url(r'^like/(?P<article_id>\d+)/$', 'mysite.article.views.like_article'),
 )
 
--- mysite/mysite/article/views.py
+++ mysite/mysite/article/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render_to_response
-from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.core.context_processors import csrf
 
 from mysite.article.models import Article
 from mysite.article.forms import ArticleForm
 
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -18,6 +18,7 @@ def article(request, article_id=1):
                              {'article': Article.objects.get(id=article_id)})
 
 
+@login_required(login_url='/accounts/login/')
 def create(request):
     if request.POST:
         form = ArticleForm(request.POST)
@@ -32,3 +33,13 @@ def create(request):
     args.update(csrf(request))
     args['form'] = form
     return render_to_response('create_article.html', args)
+
+def like_article(request, article_id):
+    if article_id:
+        a = Article.objects.get(id=article_id)
+        count = a.likes
+        count += 1
+        a.likes = count
+        a.save()
+
+    return HttpResponseRedirect('/articles/get/%s' %article_id)
