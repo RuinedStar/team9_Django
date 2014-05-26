949c7762a57e0a1ee79dd613b23f57306d57e231
arthurx94 <arthurx94@gmail.com>
2014-05-26 09:24:54 +0800

    image upload finished, admin only

--- mysite/mysite/Cachapon/models.py
+++ mysite/mysite/Cachapon/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 # Create your models here.
 class ImageTest(models.Model):
-    image1 = models.ImageField(upload_to='images/test')
+    image1 = models.ImageField(upload_to='Cachapon/images/')
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -18,4 +20,4 @@ urlpatterns = patterns('',
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-) 
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
